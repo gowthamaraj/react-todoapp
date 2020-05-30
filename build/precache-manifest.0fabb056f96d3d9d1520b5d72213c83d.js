20f351b2a683dc7762c9b30aaacc22f24352e8b1
gowthamaraj <gowthamaraj360@gmail.com>
2020-05-30 19:13:54 +0530

    feat: build added

---
+++ build/precache-manifest.0fabb056f96d3d9d1520b5d72213c83d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea7e07f7cbf81c30d8ce887eb808cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d17726cd55678ceec7b5",
+    "url": "/static/css/main.7b829ebb.chunk.css"
+  },
+  {
+    "revision": "adfccefc78cfcfb89132",
+    "url": "/static/js/2.26574634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26574634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17726cd55678ceec7b5",
+    "url": "/static/js/main.ebd205e8.chunk.js"
+  },
+  {
+    "revision": "37d41c5dd982f9fb0ad0",
+    "url": "/static/js/runtime-main.7c3665ff.js"
+  }
+]);
